c598a41d363692ebbd8f0aad8bccc97193120f31
GlowingRedSign <GlowingRedSign@users.noreply.github.com>
2016-12-11 06:00:37 -0800

    Add files via upload
    Updated endings.

--- endings.py
+++ endings.py
@@ -1,1 +1,2 @@
-endings = ["ery", "ics", "ure", "ology", "osity", "ion", "acity"]
+endings = ["ery", "ry", "ics", "ure", "ology", "osity", "ion", "acity", "ing", "ness",
+           "iness", "tion", "ction", "itude", "iveness", ""]
