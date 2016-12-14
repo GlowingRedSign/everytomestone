502e29cd81b76425c211405ab86418076d7c0125
GlowingRedSign <GlowingRedSign@users.noreply.github.com>
2016-12-14 12:31:15 -0800

    Added more endings to endings.py.

--- endings.py
+++ endings.py
@@ -1,2 +1,3 @@
-endings = ["ery", "ry", "ics", "ure", "ology", "osity", "ion", "acity", "ing", "ness",
-           "iness", "tion", "ction", "itude", "iveness", "s", ""]
+endings = ["ery", "ry", "ics", "ure", "ology", "osity", "ocity", "acity", "ation",
+           "ion", "ing", "ness", "iness", "eousness", "iousness", "tion", "ction",
+           "itude", "iture", "iveness", "ography", "ment", "onomy", "ism", ""]
