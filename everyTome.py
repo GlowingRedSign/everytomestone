65a02a951abadba62db7deb3280721955130dc55
GlowingRedSign <GlowingRedSign@users.noreply.github.com>
2016-12-14 12:30:53 -0800

    Reworked everyTome.py to update wordlist file
    Allows script to run for one iteration at a time.

--- everyTome.py
+++ everyTome.py
@@ -1,45 +1,58 @@
 import tweepy
 import time
 import random
+import os
 from secret import CONSUMER_TOKEN, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 from endings import endings
 
+def generateName(baseWord):
 
-auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-api = tweepy.API(auth)
-
-wordlist = "mergedProgress.txt"
-
-with open(wordlist, 'r') as words:
-    lines = words.readlines()
-    words.close()
-
-for line in lines:
-
-    line = line.capitalize()
-    line = line.strip()
-    baseWord = line
-    
+    name = baseWord.capitalize()
+    name = baseWord.strip()
     
     matched = 0
     for choice in endings:
         endingLength = len(choice)
         endingLength = endingLength * -1
-        strending = line[endingLength:]
+        strending = name[endingLength:]
         if choice == strending:
             matched = 1
+        
     if matched == 0:
         ending = random.choice(endings)
-        line = line + ending
+        name = name + ending
+
+    print baseWord
+    return name
+
+def postTomestone(word):
+
+    postString = "Allagan Tomestone of " + word
+    api.update_status(postString)
+    print word
     
-    try:
-        postString = "Allagan Tomestone of " + line
-        api.update_status(postString)
-        print baseWord
-    except:
-        continue
+auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+wordlist = "/home/ec2-user/everyTome/mergedProgressOneWord.txt"
+with open(wordlist, 'r') as words:
+    contents = words.readlines()
+    baseWord = contents[0]
+
+newWord = ""
+
+
+try:
+    name = generateName(baseWord)
+    postTomestone(name)
     
-    time.sleep(900)
+except:
+    newWord = contents[1]
+    postTomestone(generateName(newWord))
     
+with open(wordlist, 'w') as words:
+    for thing in contents:
+        if thing != baseWord and thing != newWord:
+            words.write(thing)
